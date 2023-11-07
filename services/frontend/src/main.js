c65df4deeb511a27092d70b01460b7108d845ed9
rmalhotra2192 <therishabhmalhotra@gmail.com>
2023-11-07 18:41:19 +0530

    primary work on frontend, also reconfigured backend and frontend Dockerfile - setup primary dependencies mainly - tailwind, popper.js, chart.js, fontawesome - setup auth pages - setup footer and navbar components - setup nginx.conf files

--- services/frontend/src/main.js
+++ services/frontend/src/main.js
@@ -3,4 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@/assets/styles/tailwind.css";
+
 createApp(App).use(store).use(router).mount("#app");
