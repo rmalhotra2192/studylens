088f348ae95b6521110a5ccafdf811b997bef7bf
rmalhotra2192 <therishabhmalhotra@gmail.com>
2023-11-16 23:40:56 +0530

    frontend design work for BookCard and BooksGrid components - added like button - added add to list button - added ratings ui - general design improvements

--- services/frontend/src/components/Cards/BookCard.vue
+++ services/frontend/src/components/Cards/BookCard.vue
@@ -1,25 +1,69 @@
 <template>
-  <div class="max-w-sm rounded overflow-hidden shadow-lg my-2 bg-white">
-    <img class="w-full" :src="bookCover" alt="Book Cover" />
-    <div class="px-6 py-4">
+  <div
+    class="max-w-sm rounded overflow-hidden shadow-lg mt-2 mb-1 bg-white relative"
+  >
+    <div class="relative">
+      <img
+        class="h-32 md:h-64 lg:h-96 object-fill w-full"
+        :src="bookCover"
+        alt="Book Cover"
+      />
+      <button
+        class="absolute top-2 right-12 opacity-80 hover:opacity-100 bg-gray-100 hover:bg-white text-gray-500 hover:text-cyan-500 font-bold py-1 px-2 rounded shadow-md transition-all duration-200 ease-in-out delay-0"
+        @click="toggleLike"
+      >
+        <i class="fas fa-plus"></i>
+      </button>
+      <button
+        class="absolute top-2 right-2 opacity-80 hover:opacity-100 bg-gray-100 hover:bg-white text-gray-500 hover:text-cyan-500 font-bold py-1 px-2 rounded shadow-md transition-all duration-200 ease-in-out delay-0"
+        @click="toggleLike"
+      >
+        <i class="fas fa-thumbs-up"></i>
+      </button>
+    </div>
+    <div class="px-6 pt-4 pb-2">
       <div class="mb-2">
-        <p class="font-bold text-xl text-left">{{ title }}</p>
+        <p class="font-bold text-lg text-left">{{ title }}</p>
         <p class="font-light text-sm text-gray-500 text-left">
           by {{ author }}
         </p>
       </div>
-      <p class="text-gray-700 text-sm text-left">
-        {{ description }}
-      </p>
+      <div class="flex items-center">
+        <div class="flex text-left text-yellow-400">
+          <template
+            v-for="index in Math.floor(rating)"
+            :key="'filled-' + index"
+          >
+            <i class="fas fa-star"></i>
+          </template>
+          <template v-if="!Number.isInteger(rating)">
+            <i class="fas fa-star-half-alt" :key="Math.floor(rating)"></i>
+          </template>
+          <template
+            v-for="index in 5 - Math.ceil(rating)"
+            :key="'outlined-' + index"
+          >
+            <i class="far fa-star"></i>
+          </template>
+        </div>
+        <span class="ml-2 text-gray-600 text-sm"
+          >({{ rating.toFixed(1) }})</span
+        >
+      </div>
     </div>
-    <div class="px-6 pt-4 pb-2">
+
+    <div class="px-6 py-2 text-left">
       <span
-        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >#fiction</span
+        class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-1 mb-1"
+        >Amazon</span
       >
       <span
-        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >#mystery</span
+        class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-1 mb-1"
+        >Barnes & Noble</span
+      >
+      <span
+        class="inline-block bg-gray-200 rounded-full px-2 py-1 text-sm font-semibold text-gray-700 mr-1 mb-1"
+        >Goodreads</span
       >
     </div>
   </div>
@@ -32,11 +76,14 @@ export default {
     bookCover: String,
     title: String,
     author: String,
-    description: String,
+    rating: Number,
+  },
+  methods: {
+    toggleLike() {
+      console.log("Like button clicked!");
+    },
   },
 };
 </script>
 
-<style scoped>
-/* You can add additional styles here if needed */
-</style>
+<style scoped></style>
--- services/frontend/src/components/Grids/BooksGrid.vue
+++ services/frontend/src/components/Grids/BooksGrid.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="container mx-auto px-4">
-    <div class="grid grid-cols-5 gap-4">
+    <div class="grid grid-cols-4 gap-4">
       <BookCard
         v-for="(book, index) in books"
         :key="index"
         :bookCover="book.cover"
         :title="book.title"
         :author="book.author"
-        :description="book.description"
+        :rating="book.rating"
       />
     </div>
   </div>
@@ -24,127 +24,32 @@ export default {
   data() {
     return {
       books: [
-        // Populate this array with objects containing cover, title, and description for each book
-        // Example:
         {
           cover:
-            "https://images.qloo.com/i/fffff616-b9ef-455e-9336-deec744dfcc8-360x504-outside.webp",
-          title: "Book Title 1",
-          author: "Someone Famous",
-          description:
-            "When Carley Sutherland receives a frantic call for help, she races to a desolate park to meet her best friend. On a dimly-lit park bench, Carley's friend fumbles in her handbag for her",
+            "https://m.media-amazon.com/images/I/A1GbblX7rOL._AC_UY218_.jpg",
+          title: "Deep Learning",
+          author: "Ian Goodfellow, Yoshua Bengio",
+          rating: 4.6,
         },
         {
           cover:
-            "https://images.qloo.com/i/ffffe0f9-1773-4d87-b6e2-d24041fe94f5-360x504-outside.webp",
-          title: "Book Title 2",
-          author: "Someone Famous",
-          description:
-            "A new adult story about breaking out, moving on, and learning that some loves never die…It’s been almost a year since that night in the attic, since something inside me died, and a new",
+            "https://m.media-amazon.com/images/I/61N9oTVT1IL._AC_UY218_.jpg",
+          title: "Deep Learning with Python, Second Edition",
+          author: "Francois Chollet",
+          rating: 4.7,
         },
         {
           cover:
-            "https://images.qloo.com/i/fffff616-b9ef-455e-9336-deec744dfcc8-360x504-outside.webp",
-          title: "Book Title 1",
-          author: "Someone Famous",
-          description:
-            "When Carley Sutherland receives a frantic call for help, she races to a desolate park to meet her best friend. On a dimly-lit park bench, Carley's friend fumbles in her handbag for her",
+            "https://m.media-amazon.com/images/I/71WzAPykhHL._AC_UF350,350_QL50_.jpg",
+          title:
+            "The Principles of Deep Learning Theory: An Effective Theory...",
+          author: " Daniel A. Roberts, Sho Yaida, Boris Hanin",
+          rating: 4.8,
         },
-        {
-          cover:
-            "https://images.qloo.com/i/ffffe0f9-1773-4d87-b6e2-d24041fe94f5-360x504-outside.webp",
-          title: "Book Title 2",
-          author: "Someone Famous",
-          description:
-            "A new adult story about breaking out, moving on, and learning that some loves never die…It’s been almost a year since that night in the attic, since something inside me died, and a new",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/fffff616-b9ef-455e-9336-deec744dfcc8-360x504-outside.webp",
-          title: "Book Title 1",
-          author: "Someone Famous",
-          description:
-            "When Carley Sutherland receives a frantic call for help, she races to a desolate park to meet her best friend. On a dimly-lit park bench, Carley's friend fumbles in her handbag for her",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/ffffe0f9-1773-4d87-b6e2-d24041fe94f5-360x504-outside.webp",
-          title: "Book Title 2",
-          author: "Someone Famous",
-          description:
-            "A new adult story about breaking out, moving on, and learning that some loves never die…It’s been almost a year since that night in the attic, since something inside me died, and a new",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/fffff616-b9ef-455e-9336-deec744dfcc8-360x504-outside.webp",
-          title: "Book Title 1",
-          author: "Someone Famous",
-          description:
-            "When Carley Sutherland receives a frantic call for help, she races to a desolate park to meet her best friend. On a dimly-lit park bench, Carley's friend fumbles in her handbag for her",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/ffffe0f9-1773-4d87-b6e2-d24041fe94f5-360x504-outside.webp",
-          title: "Book Title 2",
-          author: "Someone Famous",
-          description:
-            "A new adult story about breaking out, moving on, and learning that some loves never die…It’s been almost a year since that night in the attic, since something inside me died, and a new",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/fffff616-b9ef-455e-9336-deec744dfcc8-360x504-outside.webp",
-          title: "Book Title 1",
-          author: "Someone Famous",
-          description:
-            "When Carley Sutherland receives a frantic call for help, she races to a desolate park to meet her best friend. On a dimly-lit park bench, Carley's friend fumbles in her handbag for her",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/ffffe0f9-1773-4d87-b6e2-d24041fe94f5-360x504-outside.webp",
-          title: "Book Title 2",
-          author: "Someone Famous",
-          description:
-            "A new adult story about breaking out, moving on, and learning that some loves never die…It’s been almost a year since that night in the attic, since something inside me died, and a new",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/fffff616-b9ef-455e-9336-deec744dfcc8-360x504-outside.webp",
-          title: "Book Title 1",
-          author: "Someone Famous",
-          description:
-            "When Carley Sutherland receives a frantic call for help, she races to a desolate park to meet her best friend. On a dimly-lit park bench, Carley's friend fumbles in her handbag for her",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/ffffe0f9-1773-4d87-b6e2-d24041fe94f5-360x504-outside.webp",
-          title: "Book Title 2",
-          author: "Someone Famous",
-          description:
-            "A new adult story about breaking out, moving on, and learning that some loves never die…It’s been almost a year since that night in the attic, since something inside me died, and a new",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/fffff616-b9ef-455e-9336-deec744dfcc8-360x504-outside.webp",
-          title: "Book Title 1",
-          author: "Someone Famous",
-          description:
-            "When Carley Sutherland receives a frantic call for help, she races to a desolate park to meet her best friend. On a dimly-lit park bench, Carley's friend fumbles in her handbag for her",
-        },
-        {
-          cover:
-            "https://images.qloo.com/i/ffffe0f9-1773-4d87-b6e2-d24041fe94f5-360x504-outside.webp",
-          title: "Book Title 2",
-          author: "Someone Famous",
-          description:
-            "A new adult story about breaking out, moving on, and learning that some loves never die…It’s been almost a year since that night in the attic, since something inside me died, and a new",
-        },
-        // ...and so on for each book
       ],
     };
   },
 };
 </script>
 
-<style scoped>
-/* Styles specific to this component */
-</style>
+<style scoped></style>
