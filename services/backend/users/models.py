a6720d02f47d0b5e977c9463af379cd1f3f27aba
rmalhotra2192 <therishabhmalhotra@gmail.com>
2023-12-01 06:09:22 +0530

    Backend Work on models - added thumbnail model to base - added last_published_date field to Book model in resources - removed cover from resources base model in resources - added moderator class to represent moderators for future purposes

--- services/backend/users/models.py
+++ services/backend/users/models.py
@@ -1,12 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class Learner(AbstractUser):
-    USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
     is_test_user = models.BooleanField(default=False, blank=True)
     first_name = models.CharField(max_length=250, default="John", blank=True)
     last_name = models.CharField(max_length=250, default="Doe", blank=True)
-    email = models.CharField(max_length=500,unique=True, null=False, blank=False)
-    
+    email = models.CharField(max_length=500, unique=True, null=False, blank=False)
+
+
+class Moderator(AbstractUser):
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+    is_active = models.BooleanField(default=False, blank=True)
+    first_name = models.CharField(max_length=250, default="John", blank=True)
+    last_name = models.CharField(max_length=250, default="Doe", blank=True)
+    email = models.CharField(max_length=500, unique=True, null=False, blank=False)
