44d067490c3b72a68ad2bf0bf0c23477d412cdd9
rmalhotra2192 <therishabhmalhotra@gmail.com>
2023-12-12 06:38:41 +0530

    - formatted and cleaned entrypoint.sh

--- services/backend/entrypoint.sh
+++ services/backend/entrypoint.sh
@@ -18,18 +18,6 @@ done
 echo "Applying database migrations..."
 python manage.py migrate
 
-# Wait for Elasticsearch to be up and running
-# echo "Waiting for Elasticsearch..."
-# while ! nc -z elasticsearch 9200; do
-#   sleep 1
-# done
-# echo "Elasticsearch is up and running"
-
-# # Create the Elasticsearch indices or rebuild them
-# echo "Creating Elasticsearch indices..."
-# # python manage.py search_index --create
-# echo "y" | python manage.py search_index --rebuild
-
 # Collect static files
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
